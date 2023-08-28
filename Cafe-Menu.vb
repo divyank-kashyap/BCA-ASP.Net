e4d2f18d3aafd113333e1fb1844b788563009e79
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2023-08-28 18:19:06 +0530

    Update Cafe-Menu.vb

--- Cafe-Menu.vb
+++ Cafe-Menu.vb
@@ -5,13 +5,9 @@ Module Program
         Dim rollNo() As Integer = {1, 2, 3, 4, 5}
         Dim name() As String
         name = {"Akshay", "Anup", "Divyank", "Vikrant", "Nitish"}
-
         For i As Integer = 0 To 4 Step 1
             Console.WriteLine("My name is " & name(i) & " and my Roll Number is " & rollNo(i))
         Next
-
-
-
     End Sub
 
 End Module
