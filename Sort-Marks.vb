a934f4fa118d02cc856ee3c481e336d97784bee4
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2023-09-13 00:01:36 +0530

    Update Sort-Marks.vb

--- Sort-Marks.vb
+++ Sort-Marks.vb
@@ -1,6 +1,7 @@
 Module Program
     Sub Main(args As String())
-        Dim marks As Integer() = New Integer(10) {}
+        Dim marks As Integer() = New Integer(9) {} ' Change the size to 9 to store 10 values (0 to 9)
+
         For i = 0 To 9 Step 1
             Console.WriteLine("Enter marks for student " & (i + 1) & " : ")
             marks(i) = Integer.Parse(Console.ReadLine())
@@ -12,6 +13,5 @@ Module Program
         For i = 0 To 9 Step 1
             Console.WriteLine("Student " & (i + 1) & " : " & marks(i))
         Next
-
     End Sub
 End Module
