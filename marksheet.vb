54d63a93d90a12a8dbde8f413438e5aff22d9382
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2023-10-06 11:19:36 +0530

    Create marksheet.vb

---
+++ marksheet.vb
@@ -0,0 +1,222 @@
+<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="WebForm1.aspx.vb" Inherits="marksheetmuskan.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+ <table border="1" align="center">
+<tr>
+    <td colspan="2"><img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d8/Himachal_Pradesh_University_Shimla_Logo.svg/1200px-Himachal_Pradesh_University_Shimla_Logo.svg.png" width="150px"/> </td>
+    <td colspan="9" align="center"> Himachal Pradesh University <br>NAAC Accredited 'A' Grade University<br> Grade Card</td>
+</tr>
+
+<tr>
+    <td colspan="9"><b>Name of Examination : </b> BCA<br>
+    <b>Candidate's Name : </b>MUSKAN&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     <b>Semester : </b>Fourth&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     <br>
+     <b>Registration Number : 12345678910&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>Exam Type : </b> Fresh
+            <br />
+    <b>College Name : Govt. College Sanjauli<br>
+    <b>Exam Center Name : </b> Govt. College Sanjauli<br>
+    <b>Exam Year : </b> 2023 April<br>
+    </td>
+    <td colspan="2" align="center"><img src="https://www.gamebyte.com/wp-content/uploads/2019/11/rltaf-700x579.jpg" width="150px"/> </td>
+</tr>
+<tr>
+    <td colspan="11" align="center"><b>Paper Details </b></td>
+</tr>
+<tr>
+<th rowspan=2>S No.</th>
+<th rowspan=2>Paper Code</th>
+<th rowspan=2>Paper Name</th>
+<th rowspan=2>Category</th>
+<th colspan=3>Marks Obtained/Max Marks</th>
+<th rowspan=2>Credits</th>
+<th rowspan=2>Grade Points</th>
+<th rowspan=2>Credit Points (C*GP)</th>
+<th rowspan=2>Grade</th>
+</tr>
+
+<tr>
+<td > <b>CCA</b></td>
+<td ><b>ESE</b></td>
+<td><b>Total</b></td>
+</tr>
+
+<tr>
+    <th rowspan=2>1</th>
+    <td rowspan=2>BCA0401</td>
+   <td rowspan=2>Personal Management</td>
+   <td rowspan=2>Major</td>
+   <td > 26</td>
+   <td > 48</td>
+  <td >74</td>
+  <td rowspan=2>4</td>
+  <td rowspan=2>7.5</td>
+  <td rowspan=2>30.0</td>
+ <td rowspan=2>A++</td>
+</tr>
+<tr>
+    <td>30</td>
+    <td>70 </td>
+    <td>100</td>
+</tr>
+
+<tr>
+    <th rowspan=2>2</th>
+    <td rowspan=2>BCA0402</td>
+   <td rowspan=2>Accounting</td>
+   <td rowspan=2>Major</td>
+   <td > 28</td>
+   <td > 48</td>
+  <td >76</td>
+  <td rowspan=2>4</td>
+  <td rowspan=2>8.0</td>
+  <td rowspan=2>32.0</td>
+ <td rowspan=2>O</td>
+</tr>
+<tr>
+    <td>30</td>
+    <td>70 </td>
+    <td>100</td>
+</tr>
+
+<tr>
+    <th rowspan=2>3</th>
+    <td rowspan=2>BCA0403</td>
+   <td rowspan=2>System Analysis and Design</td>
+   <td rowspan=2>Major</td>
+   <td > 25</td>
+   <td > 48</td>
+  <td >73</td>
+  <td rowspan=2>4</td>
+  <td rowspan=2>7.5</td>
+  <td rowspan=2>30.0</td>
+ <td rowspan=2>A++</td>
+</tr>
+<tr>
+    <td>30</td>
+    <td>70 </td>
+    <td>100</td>
+</tr>
+
+
+<tr>
+    <th rowspan=2>4</th>
+    <td rowspan=2>BCA0404</td>
+   <td rowspan=2>Internet Technology &
+       <br />WebPage Design</td>
+   <td rowspan=2>Major</td>
+   <td > 26</td>
+   <td > 57</td>
+  <td >83</td>
+  <td rowspan=2>4</td>
+  <td rowspan=2>8.5</td>
+  <td rowspan=2>34.0</td>
+ <td rowspan=2>A++</td>
+</tr>
+<tr>
+    <td>30</td>
+    <td>70 </td>
+    <td>100</td>
+</tr>
+
+<tr>
+    <th rowspan=2>5</th>
+    <td rowspan=2>BCA0404(P)</td>
+   <td rowspan=2>Internet Technology & 
+      <br />WebPage Design Lab VII</td>
+   <td rowspan=2>Major Lab</td>
+   <td > 12</td>
+   <td > 31</td>
+  <td >43</td>
+  <td rowspan=2>3</td>
+  <td rowspan=2>9.0</td>
+  <td rowspan=2>27.0</td>
+ <td rowspan=2>O++</td>
+</tr>
+<tr>
+    <td>15</td>
+    <td>35</td>
+    <td>50</td>
+</tr>
+
+<tr>
+    <th rowspan=2>6</th>
+    <td rowspan=2>BCA0405</td>
+   <td rowspan=2>Programming In Visual Basic</td>
+   <td rowspan=2>Major</td>
+   <td > 29</td>
+   <td > 44</td>
+  <td >73</td>
+  <td rowspan=2>4</td>
+  <td rowspan=2>7.5</td>
+  <td rowspan=2>30.0</td>
+ <td rowspan=2>A++</td>
+</tr>
+<tr>
+    <td>30</td>
+    <td>70 </td>
+    <td>100</td>
+</tr>
+
+<tr>
+    <th rowspan=2>7</th>
+    <td rowspan=2>BCA0405(P)</td>
+   <td rowspan=2>Programming In Visual
+       <br />Basic Lab VII</td>
+   <td rowspan=2>Major Lab</td>
+   <td > 13</td>
+   <td > 33</td>
+  <td >46</td>
+  <td rowspan=2>3</td>
+  <td rowspan=2>9.5</td>
+  <td rowspan=2>28.5</td>
+ <td rowspan=2>S</td>
+</tr>
+<tr>
+    <td>15</td>
+    <td>35</td>
+    <td>50</td>
+</tr>
+      
+     <tr>
+         <td colspan="4" rowspan="2" align="Center">Total</td>
+          <td >-</td>
+          <td >-</td>
+          <td >468</td>
+          <td rowspan="2">26</td>
+          <td rowspan="2">-</td>
+          <td rowspan="2">211.50</td>
+          <td rowspan="2">-</td>
+     </tr>
+     <tr>
+         <td >-</td>
+         <td >-</td>
+         <td >600</td>
+     </tr>
+     <tr>
+         <td rowspan="1" colspan="3" align="Center">Semester Grade Point Average(SGPA) : 8.13</td>
+         <td rowspan="1" colspan="5" align="center">Cumulative Grade Point Average(CGPA)</td>
+         <td colspan="3" rowspan="1" align="Center">Semester Grade :           O+</td>
+     </tr>
+     <tr>
+         <td colspan="6" rospan="6" padding="20px">
+             <b>Date of Result Declaration :</b>04 September 2023<br />
+             <b>Date of Result Settlement :</b>04 September 2023<br />
+             <b>Verified by :</b>AR Examination-II, Himachal Pradesh University<br />
+             <b>Date of Grade Card Download :</b> 04 September 2023 05:43:06PM<br />
+         </td>
+
+         <td colspan="6" rospan="5" align="center"> <img src="https://qph.cf2.quoracdn.net/main-qimg-a3d3d731df0bee84b5ae58c41cacc2e8-lq" width=" 150px" /><br />
+             <b>Signature of Controlller of Examination</b>
+         </td>
+
+     </tr>
+</table>
+</body>
+</html>
